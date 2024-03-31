d8459a2b447124219703311c418a622338b2e9f1
Vicoutinho <vitoriacoutinhosilva12@gmail.com>
2024-03-30 21:45:43 -0300

    Projeto feito em aula para treinar o comando Jpanel no Java

---
+++ src/main/java/com/mycompany/cadastropet/CadastroPet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastropet;
+
+/**
+ *
+ * @author Vitoria
+ */
+public class CadastroPet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
